2bf2ade97e5bee8ff3d6e0a3be3b3017afd448bb
Harry Gustave <harry23gust@gmail.com>
2021-02-14 22:49:10 -0600

    new password generator to include checkbox and range

---
+++ css/styles.css
@@ -0,0 +1,18 @@
+body {
+    background-color: black;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    margin: 0;
+    font-family: cursive;
+  }
+  
+  .container {
+    background-color:rgba(29, 167, 36, 0.842);
+    padding: 2rem;
+    border-radius: 1rem;
+    border: 2px solid black;
+    
+  }
