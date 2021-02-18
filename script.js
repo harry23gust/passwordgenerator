58795a03fe781c05b0ad202ffd37b3c9107b4961
Harry Gustave <harry23gust@gmail.com>
2021-02-17 18:37:28 -0600

    added eventlistener

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-body {
-    background-color: black;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    margin: 0;
-    font-family: cursive;
-  }
+const characterAmountRange = document.getElementById("characterAmountRange")
+
+const characterAmountNumber = document.getElementById("characterAmountNumber")
+const form = document.getElementById("characterAmountRange")
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+
+characterAmountNumber.addEventListener("input", syncCharacterAmount)
+characterAmountRange.addEventListener("input", syncCharacterAmount)
+
+form.addEventListener('submit', e=> {
+  e.preventDefault()
+  const password = generatePassword(characterAmountNumber, characterAmountRange,  includeNumbers, includeSymbols, includeUppercase,)
+  const characterAmount = characterAmountNumber.value
+}
+
+function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase) {
+  ["a". "b", "c", "d", "e", "f", "g", "h", "y", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",]
   
-  .container {
-    background-color:rgba(29, 167, 36, 0.842);
-    padding: 2rem;
-    border-radius: 1rem;
-    border: 2px solid black;
-    
-  }
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value;
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  characterAmountNumber.value = value;
+  characterAmountRange.value = value;
+}
